b6fccddf840f1771e6353d964e480030df3aa3ea
danielrbaran <dan@danielbaran.com>
2020-01-21 12:12:23 -0800

    small change to main file

--- alien_invasion.py
+++ alien_invasion.py
@@ -99,7 +99,7 @@ class AlienInvasion:
 								(3 * alien_height) - ship_height)
 		number_rows = available_space_y // (2* alien_height)
 
-		# create full fleet of aliens
+		# create full fleet of motherfucking aliens
 		for row_number in range(number_rows):
 			for alien_number in range(number_aliens_x):
 				self._create_alien(alien_number, row_number)
